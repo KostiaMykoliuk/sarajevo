f7d07c3122dbfde233ea60bf6b4df7a2247bf813
Kostia Mykoliuk <56024909+KostiaMykoliuk@users.noreply.github.com>
2021-02-09 21:33:15 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,46 +1,3 @@
-
-let filter = (works,filtter_name) =>{
-	if (filtter_name) {
-		let filtter_name_length = Object.keys(filtter_name).length;
-		for (let i = filtter_name_length - 1; i >= 0; i--) {
-			name = filtter_name[i];
-			let works_length = Object.keys(works[name]).length;
-			console.log(works_length)
-			for (let i = works_length - 1; i >= 0; i--) {
-				console.log(works[name][i].img_url)
-				document.getElementById('ourWorks').insertAdjacentHTML('beforeend', '<div class="img_wrapper"><img class="img" src="'+works[name][i].img_url+'" alt="'+works[name][i].name+'"><div class="work_hover"><i class="far fa-eye"></i><span class="title">'+works[name][i].name+'</span><span class="category">'+works[name][i].category+'</span></div></div>');
-			}
-		}
-	}else{
-		let works_length = Object.keys(works).length;
-		for (let i = works_length - 1; i >= 0; i--) {
-			console.log(works[i].img_url)
-			document.getElementById('ourWorks').insertAdjacentHTML('beforeend', '<div class="img_wrapper"><img class="img" src="'+works[i].img_url+'" alt="'+works[i].name+'"><div class="work_hover"><i class="far fa-eye"></i><span class="title">'+works[i].name+'</span><span class="category">'+works[i].category+'</span></div></div>');
-		}
-	}
-}
-
-
-let database = (filterName) =>{
-	let works,
-		filtter_name;
-
-	fetch('database/our_work.json')
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-			if (filterName === 'all') {
-				filtter_name = data.works.filtter_name
-				works = data.works
-				filter(works, filtter_name)
-			}else{
-				works = data.works[filterName]
-				filter(works)
-			}
-		})
-}
-
 let deleteAll = () => {
 	let element = document.getElementById('ourWorks');
     while (element.firstChild) {
@@ -48,18 +5,32 @@ let deleteAll = () => {
     }
 }
 
+let filter = (works,category) =>{
+	deleteAll()
+	if (works) {
+		let works_length = Object.keys(works).length;
+		for (var i = works_length-1; i >= 0; i--) {
+			if(category === 'all'){
+				document.getElementById('ourWorks').insertAdjacentHTML('beforeend', '<div class="img_wrapper"><img class="img" src="'+works[i].img_url+'" alt="'+works[i].name+'"><div class="work_hover"><i class="far fa-eye"></i><span class="title">'+works[i].name+'</span><span class="category">'+works[i].category+'</span></div></div>');
+			}else{
+				if (works[i].category === category) {
+				document.getElementById('ourWorks').insertAdjacentHTML('beforeend', '<div class="img_wrapper"><img class="img" src="'+works[i].img_url+'" alt="'+works[i].name+'"><div class="work_hover"><i class="far fa-eye"></i><span class="title">'+works[i].name+'</span><span class="category">'+works[i].category+'</span></div></div>');
+				}
+			}
+		}
+	}
+	
+}
 
-
-let filterMenu = () =>{
+let filterMenu = (works) =>{
 	let item_array = document.querySelectorAll('#fillterMenu li')
 		active_fillter = 0;
 		item_array = Array.from(item_array);
-
+	filter(works, 'all');
 	document.querySelector('#fillterMenu').addEventListener('click', (e)=>{
 		if (e.target.tagName === 'LI') {
-			let filterName = e.target.getAttribute('data-fillter');
-			deleteAll()
-			database(filterName);
+			let category = e.target.getAttribute('data-fillter');			
+			filter(works,category);
 			item_array[active_fillter].classList.remove('active');
 			active_fillter = item_array.indexOf(e.target);
 			item_array[active_fillter].classList.add('active');
@@ -67,22 +38,47 @@ let filterMenu = () =>{
 	})
 }
 
+let database = () =>{
+	let works;
 
+	fetch('database/our_work.json')
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			if (data) {
+				works = data.works
+				filterMenu(works)
+			}
+		})
+}
 
+let pageScroll = () =>{
+	document.getElementById('mainMenu').addEventListener('click', (e)=>{
+		e.preventDefault(); 
+  		let menu_id = e.target.getAttribute('data-menu'); 
+  			if (menu_id) {
+  				let id = document.getElementById(menu_id);
 
-document.addEventListener('DOMContentLoaded', function(){
-	filterMenu();
-	database('all');
+  				id.scrollIntoView({block: "start", behavior: "smooth"});
+  			}
+	});
+}
 
-	document.getElementById('openMemu').addEventListener('click', ()=>{
+let mobailMenu = () =>{
+	document.getElementById('openMenu').addEventListener('click', ()=>{
 		document.getElementById('menuWrapper').style.display = 'flex';
-		document.getElementById('menuWrapper').style.background = '#333';
-		document.getElementById('menuWrapper').style.zIndex = '2';
 	})
-
 	document.getElementById('menuWrapper').addEventListener('click', ()=>{
 		document.getElementById('menuWrapper').style.display = 'none';
 	})
-		
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+	database();
+	pageScroll();
+	if (screen.width < 992) {
+		mobailMenu();
+	}
 
 });
